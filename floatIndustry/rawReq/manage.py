071d0da5dca15357a8dab35151ae86f159968d6b
Live session user <ved.harish3@gmail.com>
2013-09-25 03:12:16 +0000

    All working

--- floatIndustry/rawReq/manage.py
+++ floatIndustry/rawReq/manage.py
@@ -4,24 +4,12 @@ from django.core.context_processors import csrf
 from rawReq import models
 
 def index(request):
-    a1 = models.rawMatLists.objects.create(listName='listName1', forColor='colorname1')
-    a1.save()
-    b2 = models.rawMat.objects.create(name='name11')
-    b2.save()
-    b2.rawMatList.add(a1)
-    #b1 = models.rawMat()
-    #b1.name = 'silica'
-    #b1.save()
-    #a1 = models.rawMatLists(listNAme='qwqw', forColor='qsaqas')
-    #a1.save()
-    #b1 = models.rawMat(name='police')
-    #b1.save()
     if request.method == 'POST':
         number_fields = int(request.POST['numberOfFields']) - 1
         name_of_list = request.POST['name_of_list']
         color_of_glass = request.POST['color_of_glass']
         
-        list_object = models.rawMatLists().createList(list_name=name_of_list, color_name=color_of_glass)
+        list_object = models.rawMatLists.objects.create(listName=name_of_list, forColor=color_of_glass)
         
         context = {}
         rawMatNamesList = []
@@ -30,8 +18,8 @@ def index(request):
             if request.POST['rawMatName'+str(x+1)] != '':
                 tempList = [request.POST['rawMatName'+str(x+1)], 'quantity'+request.POST['rawMatName'+str(x+1)]]
                 rawMatNamesList.append(tempList)
-                raw_material_object = models.rawMat().createRawMat(request.POST['rawMatName'+str(x+1)])
-                raw_material_object.addList(list_object)
+                raw_material_object = models.rawMat.objects.create(name=request.POST['rawMatName'+str(x+1)])
+                raw_material_object.rawMatList.add(list_object)
         
         context.update({'rawMatNames': rawMatNamesList})
         return render_to_response('createCurrentReq.html', context)
--- floatIndustry/rawReq/models.py
+++ floatIndustry/rawReq/models.py
@@ -4,19 +4,6 @@ class rawMatLists(models.Model):
     listName = models.CharField(max_length=15)
     forColor = models.CharField(max_length=15)
 
-    def createList(self, list_name, color_name):
-        self.listName = list_name
-        self.forColor = color_name
-        self.save()
-        return self
-
 class rawMat(models.Model):
     name = models.CharField(max_length=40)
     rawMatList = models.ManyToManyField(rawMatLists)
-
-    def createRawMat(self, name):
-        self.name = name
-        return self
-
-    def addList(self, list_name):
-        self.rawMatList.add(list_name)
